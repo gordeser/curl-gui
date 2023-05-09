e1214ec4c171efb988f7e04194fe202cc9ab1f31
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-09 17:37:12 +0200

    Added new label for download url. Updated positions of labels

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ class Application(tk.Tk):
         self.title("cURL GUI")
         self.geometry("500x500")
 
-        self.label = tk.Label(self, text="cURL GUI", font=("Helvetica", 20, "bold"))
-        # self.label.grid(padx=10, pady=10, sticky="n")
-        self.label.pack(pady=20)
+        self.label_title = tk.Label(self, text="cURL GUI", font=("Arial", 20, "bold"))
+        self.label_title.place(x=175, y=10)
+
+        self.label_download = tk.Label(self, text="Download URL: ", font=("Arial", 11))
+        self.label_download.place(x=10, y=100)
+
+
 
 
 if __name__ == "__main__":
