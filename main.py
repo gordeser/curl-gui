c41cb39ee79bb0e3af9ee29b38b46cb894548bcb
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-09 17:23:46 +0200

    Added label with name of programm

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ class Application(tk.Tk):
         super().__init__()
 
         self.title("cURL GUI")
-        self.geometry('500x500')
+        self.geometry("500x500")
+
+        self.label = tk.Label(self, text="cURL GUI", font=("Helvetica", 20, "bold"))
+        # self.label.grid(padx=10, pady=10, sticky="n")
+        self.label.pack(pady=20)
 
 
 if __name__ == "__main__":
