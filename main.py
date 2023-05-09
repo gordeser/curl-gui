bb20428211755a8dd2f84136b7ff4a877f8f2a37
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-09 17:55:23 +0200

    Create input for link and button for download

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ class Application(tk.Tk):
         self.label_download = tk.Label(self, text="Download URL: ", font=("Arial", 11))
         self.label_download.place(x=10, y=100)
 
+        self.input_download = tk.Entry(self, width=50)
+        self.input_download.place(x=150, y=100)
+
+        self.button_download = tk.Button(self, text="Download", width=10)
+        self.button_download.place(x=200, y=200)
 
 
 
 if __name__ == "__main__":
     app = Application()
     app.mainloop()
-
-
