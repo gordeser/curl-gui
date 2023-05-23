343f51256adff589d938f33bcc59adfe3261d9a1
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-23 11:41:23 +0200

    Create all elements for simple download file using curl

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 from tkinter import ttk
 
 import subprocess
+import re
 
 
 class Application(tk.Tk):
@@ -16,18 +17,26 @@ class Application(tk.Tk):
 
         self.make_window()
 
+        # variables
+        self.text_input_download = tk.StringVar()
+        self.text_input_download.set("http://")
+
+        # labels
         self.label_title = tk.Label(self, text="cURL GUI", font=("Arial", 20, "bold"))
-        self.label_title.place(x=175, y=10)
+        self.label_download_url = tk.Label(self, text="Download URL: ", font=("Arial", 11))
+        self.label_path = tk.Label(self, text="Path: ", font=("Arial", 11))
 
-        self.label_download = tk.Label(self, text="Download URL: ", font=("Arial", 11))
-        self.label_download.place(x=10, y=100)
+        # entries
+        self.input_download = tk.Entry(self, width=50, textvariable="self.text_input_download")
+        self.input_path = tk.Entry(self, width=50)
 
-        self.input_download = tk.Entry(self, width=50)
-        self.input_download.place(x=150, y=100)
+        # buttons
+        self.button_select_path = tk.Button(self, text="Choose folder", command=self.ask_for_directory)
+        self.button_download = tk.Button(self, text="Download", width=10, command=self.download_file)
 
-        self.button_download = tk.Button(self, text="Download", width=10)
-        self.button_download.place(x=200, y=200)
-
+        # other set functions
+        self.set_positions()
+        self.set_binds()
 
 
 if __name__ == "__main__":
