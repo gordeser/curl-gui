9519e7ac4a8fbc85141fcdda12bc1ea784001aa1
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-25 09:50:34 +0200

    Designed ProxyWindow

--- main.py
+++ main.py
@@ -277,17 +277,49 @@ class DebugWindow(tk.Toplevel):
 
 
 class ProxyWindow(tk.Toplevel):
-    # todo: http(s) and socks5 proxy, login, password, ip, port
     def make_window(self):
         self.title("Set Proxy")
-        self.geometry("300x300")
+        self.geometry("300x175")
         self.resizable(False, False)
         self.withdraw()
+
+    def set_positions(self):
+        self.label_protocol.place(x=10, y=10)
+        self.label_hostname.place(x=10, y=40)
+        self.label_port.place(x=10, y=70)
+        self.label_username.place(x=10, y=100)
+        self.label_password.place(x=10, y=130)
+
+        self.input_hostname.place(x=100, y=40)
+        self.input_port.place(x=100, y=70)
+        self.input_username.place(x=100, y=100)
+        self.input_password.place(x=100, y=130)
+
+        self.combobox_protocol.place(x=100, y=10)
+
+    def set_currents(self):
+        self.combobox_protocol.current(0)
 
     def __init__(self, parent):
         super().__init__(parent)
 
         self.make_window()
+
+        self.label_protocol = tk.Label(self, text="Protocol")
+        self.label_hostname = tk.Label(self, text="Hostname")
+        self.label_port = tk.Label(self, text="Port")
+        self.label_username = tk.Label(self, text="Username")
+        self.label_password = tk.Label(self, text="Password")
+
+        self.input_hostname = tk.Entry(self, width=30)
+        self.input_port = tk.Entry(self, width=30)
+        self.input_username = tk.Entry(self, width=30)
+        self.input_password = tk.Entry(self, width=30)
+
+        self.combobox_protocol = ttk.Combobox(self, values=['HTTP', 'SOCKS5'], width=10, state="readonly")
+
+        self.set_positions()
+        self.set_currents()
 
 
 if __name__ == "__main__":
