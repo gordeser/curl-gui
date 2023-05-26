0c68714155154bb562d6ec62e293128f09658b52
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-26 18:49:04 +0200

    Fix previous commit

---
+++ src/windows/debug.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+from tkinter import ttk
+
+
+class DebugWindow(tk.Toplevel):
+
+    def make_window(self):
+        self.title("Debug mode")
+        self.geometry("805x500")
+        self.resizable(False, False)
+        self.withdraw()
+
+    def set_positions(self):
+        self.checkbutton_verbose.place(x=20, y=460)
+        self.button_export_debug.place(x=650, y=460)
+        self.button_clear_logs.place(x=350, y=460)
+        self.text_logs.place(x=0, y=0)
+
+    def clear_logs(self):
+        self.text_logs.config(state=tk.NORMAL)
+        self.text_logs.delete("1.0", tk.END)
+        self.text_logs.config(state=tk.DISABLED)
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.make_window()
+
+        self.checkbutton_verbose = ttk.Checkbutton(self, text="Enable verbose mode", variable=parent.verbose)
+
+        self.button_export_debug = tk.Button(self, text="Export logs to file", command=parent.export_debug)
+        self.button_clear_logs = tk.Button(self, text="Clear logs", command=self.clear_logs)
+
+        self.text_logs = tk.Text(self, state=tk.DISABLED, width=100, height=25)
+
+        self.set_positions()
