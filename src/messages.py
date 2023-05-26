4be192c2d47e627e2fe1f9b1c7db8ea6f30be107
Sergei Gordeev <gordeser@fit.cvut.cz>
2023-05-26 18:48:22 +0200

    From one file to many

---
+++ src/messages.py
@@ -0,0 +1,9 @@
+from tkinter import messagebox
+
+
+def show_error(message):
+    messagebox.showerror("Error", message)
+
+
+def show_info(message):
+    messagebox.showinfo("Information", message)
